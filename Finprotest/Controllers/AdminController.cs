7ad7b95234fd87feb1d08153127bcbced21bc1e9
tikhmanulhakim <94417999+tikhmanulhakim@users.noreply.github.com>
2023-04-04 11:22:04 +0700

    finpro selasa pagi

---
+++ Finprotest/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Finprotest.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Dashboard()
+        {
+            //if (Session["id_admin"] != null)
+            //{
+                
+            //}
+            //else
+            //{
+            //    return RedirectToAction("LoginAdmin", "Login");
+            //}
+            return View();
+        }
+    }
+}
